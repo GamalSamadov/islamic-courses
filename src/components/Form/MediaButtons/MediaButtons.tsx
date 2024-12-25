f87eb1c9fe81bf24bc53587b8fea912820b6ed41
Jamoliddin Samadov <gamalsamadov@gmail.com>
2024-12-25 12:21:20 +0300

    Login register: icons fixed 👨🏼‍💻

--- src/components/Form/MediaButtons/MediaButtons.tsx
+++ src/components/Form/MediaButtons/MediaButtons.tsx
@@ -1,14 +1,17 @@
 import { HrWithTitle } from '@/components/shared/ui/Form/HrWIhTItle/HrWithTitle'
 import styles from './MediaButtons.module.scss'
 import { GoogleButton } from '@/components/shared/ui/Form/GoogleButton/GoogleButton'
+import { useRouter } from 'next/navigation'
 
 interface Props {
 	isLoading: boolean
 }
 
 export const MediaButtons = ({ isLoading }: Props) => {
+	const router = useRouter()
+
 	return (
-		<div className={styles.media}>
+		<div className={styles.media} onClick={() => router.push('/auth/google')}>
 			<HrWithTitle title='Or' />
 			<GoogleButton isLoading={isLoading} />
 		</div>
